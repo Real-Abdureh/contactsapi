648179d2b81349c1fb71a39b90af142d34ccdef6
Real-Abdureh <abdouliabbas75@gmail.com>
2023-03-07 23:28:47 +0100

    contacts app

---
+++ contacts/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+from .models import contacts
+
+
+class ContactSerializer(ModelSerializer):
+
+    class Meta:
+        model=contacts
+
+    fields = ['country_code', 'first_name', 'last_name', 'phone_number', 'contact_picture', 'is_favourite']
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import contacts
+from .serializers import ContactSerializer
+from rest_framework import permissions
+
+
+class ContactList(ListCreateAPIView):
+
+    serializer_class = ContactSerializer
+    permission_classes = (permissions.IsAuthenticated)
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner= self.request.user)
+
+    def get_queryset(self):
+        return contacts.objects.filter(owner= self.request.user)
+
+
+class ContactDetailView(RetrieveUpdateDestroyAPIView):
+
+    serializer_class = ContactSerializer
+    permission_classes = (permissions.IsAuthenticated)
+    lookup_field = 'id'  
+
+
+    def get_queryset(self):
+        return contacts.objects.filter(owner= self.request.user)
 
 # Create your views here.
